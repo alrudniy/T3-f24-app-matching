6d15f6826ee335cf526928e819499ad222d88c54
Jared Simonetti <jaredsimonetti2@gmail.com>
2024-11-04 13:01:56 -0500

    Fix: Profile txt 'text'

--- frontend/app/(main)/(home)/profile.tsx
+++ frontend/app/(main)/(home)/profile.tsx
@@ -2,6 +2,7 @@ import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { Link } from "expo-router";
 import { styles1 } from "./styles";
+import {Text} from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function ProfileView() {
@@ -11,7 +12,7 @@ export default function ProfileView() {
         <SafeAreaView style={styles1.innerContainer}>
           <ThemedText type="title">Profile</ThemedText>
 
-          <text>Profile information here</text>
+          <Text>Profile information here</Text>
 
           <Link style={styles1.link} href="/(main)/(home)/matching">
             Go to matching
